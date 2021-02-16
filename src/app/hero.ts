40d6d375f15cfa6781e94cbd113d7e1da4d62225
petern1953 <petern1953@gmail.com>
2021-02-16 10:20:29 +0100

    addition to hero class to kill filter errormessage

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,5 @@
 export class Hero {
+  [index: string]: string | number;
   id: number = 0;
   name: string = '';
   superPower: string = '';
--- src/app/pipe/filter.pipe.ts
+++ src/app/pipe/filter.pipe.ts
@@ -6,9 +6,13 @@ import { Hero } from '../hero';
 })
 export class FilterPipe implements PipeTransform {
 
+  // transform(value: Hero[] | null, phrase: string, key: string | number): any {
   transform(value: Hero[] | null, phrase: string, key: string | number): any {
     if (!Array.isArray(value) || !phrase || !key)
       return value;
+
+    if (key == "id")
+      return value.filter(item => ('' + item.id) == (phrase));
 
     let lcPhrase = ('' + phrase).toLowerCase();
     return value.filter(item => ('' + item[key]).toLowerCase().includes(lcPhrase));
